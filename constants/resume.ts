dcd93ed4b29d382aab6082559e07607cd4d92471
foxyfool <rahulkumarmaity9@gmail.com>
2025-03-03 13:26:03 +0530

    resume

--- constants/resume.ts
+++ constants/resume.ts
@@ -1,4 +1,4 @@
 export const resume_link =
-  "https://drive.google.com/file/d/1T0rmRxGsauwFnUDO81Rw0ox_53WsSdox/view?usp=sharing";
+  "https://drive.google.com/file/d/1JEcJsNJ9Bs4eCg5IUjxMhrFu2Fq-Mb-U/view?usp=sharing";
 
 export default resume_link;
